72495c1b4e7a3bb407b30605be55d305fc219a2f
codethread <codethread@users.noreply.github.com>
2025-06-04 05:13:24 +0000

    deploy: 041377f2c584f4b8e28e56c294215d384738ce3c

--- assets/index-Bs-hgVPp.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Dv9qvSYU.js";const e=function(){return o.jsx("div",{children:'Hello "root"!'})};export{e as component};
---
+++ assets/index-DkiE35E2.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-tOR3AC1v.js";const e=function(){return o.jsx("div",{children:'Hello "root"!'})};export{e as component};
